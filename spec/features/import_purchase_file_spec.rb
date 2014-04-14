9cb2ffcde6467dd6f5ecfe07361fe043b3deaae1
Carlisia Campos <carlisia@grokkingtech.io>
2014-04-13 23:23:14 -0700

    Add more feature test

--- spec/features/import_purchase_file_spec.rb
+++ spec/features/import_purchase_file_spec.rb
@@ -9,19 +9,23 @@ feature 'User attempts to import a file and' do
 
   scenario 'sees a report of only the imported file when the all the data in the file is valid' do
     visit root_path
+    purchase_count = Purchase.count
     file_path = Rails.root + "spec/fixtures/purchase_data.tsv"
     attach_file('file', file_path)
     click_on 'Import File'
     expect(page).to have_content('95.0')
     expect(page).not_to have_content('All Reports List')
+    expect(Purchase.count).to eq purchase_count + 4
   end
 
   scenario 'gets an error message when file is invalid' do
     visit root_path
+    purchase_count = Purchase.count
     file_path = Rails.root + "spec/fixtures/purchase_data-invalid.tsv"
     attach_file('file', file_path)
     click_on 'Import File'
     page.should have_content('error prohibited this import from completing:')
+    expect(Purchase.count).to eq purchase_count
   end
 
   scenario "gets an alert message when no file is selected" do
